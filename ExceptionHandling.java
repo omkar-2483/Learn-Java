dfaa22ac58b0142de863670302c89ce449a9a9af
Omkar Kasture <154991981+omkar-2483@users.noreply.github.com>
2024-01-28 10:26:41 +0530

    Add files via upload

--- ExceptionHandling.java
+++ ExceptionHandling.java
@@ -1,46 +1,43 @@
-package problems;
+package assignment5;
+import java.util.Scanner;
+
+class Data{
+	String name;
+	String city;
+	String has4wheeler;
+
+	Scanner sc=new Scanner(System.in);
+	public void getData() {
+		try {
+			System.out.println("enter your name: ");
+			name=sc.next();
+			System.out.println("enter your city: ");
+			city=sc.next();
+			System.out.println("Do you have four wheeler?(enter yes/no): ");
+			has4wheeler=sc.next();
+		}
+		catch(Exception e) {
+			System.out.println("Enter correct input");
+		}
+	}
+	
+	public void showResult() throws Exception {
+		System.out.println("City= "+city);
+		if((city.toLowerCase().equals("pune")||city.toLowerCase().equals("mumbai")||city.toLowerCase().equals("banglore")||city.toLowerCase().equals("chennai"))&& has4wheeler.toLowerCase().equals("yes")) {
+			System.out.println("criteria matched....");
+		}
+		else {
+			throw new Exception("criteria does not match...");
+		}
+	}
+}
 
 public class ExceptionHandling {
-
-	public static void main(String[] args) {
-		int[] string= {0,1,2,3,4,5};
-		
-		
-		//array out of bound exception handling
-		try {
-			for(int i=0;true;i++)
-			{
-				System.out.print(string[i]+" ");
-			}
+	public static void main(String[] args) throws Exception {
+		while(true) {
+			Data person=new Data();
+		    person.getData();
+		    person.showResult();
 		}
-		catch(Exception e) {
-			System.out.println("ArrayIndexOutOfBoundException");
-		}
-		finally{
-			System.out.println(" exception-1 code ended");
-		}
-		
-		
-		//arithmetic exception handling
-		
-		try
-		{
-			int c=126/0;
-		}
-		catch(Exception e) {
-			System.out.println("ArithmeticException");
-		}
-		
-		//null pointer exception
-		String s=null;
-		//s="omkar";
-		if(s==null)
-		{
-			throw new NullPointerException("string is not initialized");
-		}
-		else{
-			System.out.println("length of string is: "+s.length());
-		}
-		
 	}
 }
