f29e8be6d67add46c234d4c3c1ea8dd00c2b99cf
omkar-2483 <kastureomkar03@gmail.com>
2024-02-06 09:29:37 +0530

    new interesting thing in java

---
+++ OctalHexadecimal.java
@@ -0,0 +1,12 @@
+// java supports octal and hexadecimal integers
+// octal numbers have prefix 0
+// hexadecimal have prefox 0X
+
+class OctalHexadecimal{
+    public static void main(String[] args){
+        int num=010-02;
+        System.out.println(num);
+        int num2=0X10-0XF;
+        System.out.print(num2);
+    }
+}
