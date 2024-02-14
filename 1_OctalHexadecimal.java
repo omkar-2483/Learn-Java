a51ec07149fec9676b5d08a7a77d752dca018f38
omkar-2483 <kastureomkar03@gmail.com>
2024-02-14 17:55:34 +0530

    1_OctalHexadecimal

--- 1_OctalHexadecimal.java
+++ 1_OctalHexadecimal.java
@@ -1,12 +1,15 @@
-// java supports octal and hexadecimal integers
+// java supports octal and hexadecimal and binary integers
 // octal numbers have prefix 0
 // hexadecimal have prefox 0X
+//binary have prefix 0b
 
 class OctalHexadecimal{
     public static void main(String[] args){
         int num=010-02;
         System.out.println(num);
-        int num2=0X10-0XF;
-        System.out.print(num2);
+        int num2=0X10-0X1;
+        System.out.println(num2);
+        int num3=0b1010;
+        System.out.print(num3);
     }
 }
